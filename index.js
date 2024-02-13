d4d12b175ab4770778558f7e51fa3d314f0b0342
Daniel <deslandev@gmail.com>
2024-02-13 12:59:04 -0300

    chore: dotenv setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const name = ''
+import express from 'express'
+import 'dotenv/config.js'
+
+import { pool } from './src/db/postgres/helper.js'
+
+const app = express()
+app.get('/', async (req, res) => {
+    const client = await pool.connect()
+    const results = await client.query('SELECT * FROM users;')
+
+    res.send(JSON.stringify(results.rows))
+})
+
+app.listen(3000, () => console.log('listening on port 3000'))
